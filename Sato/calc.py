c818473bff5d98c7299f03a143a96530806c4556
AkiraTasuku <sato.akira.nt@mynavi.jp>
2023-05-18 05:03:00 +0000

    calc.pyの追加

---
+++ Sato/calc.py
@@ -0,0 +1,12 @@
+import sys
+args = sys.argv
+input1 = args[1]
+input2 = args[2]
+input3 = args[3]
+input1 = int(input1)
+input2 = int(input2)
+input3 = int(input3)
+
+sum = input1 + input2 + input3
+
+print(sum, end="")

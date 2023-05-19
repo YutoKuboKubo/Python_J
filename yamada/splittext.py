ca9d366d96eaa9e77a6e8c397846d56d09c27550
ShomaYamada11 <yamada.shoma.db@mynavi.jp>
2023-05-19 06:41:20 +0000

    splittextを修正

--- yamada/splittext.py
+++ yamada/splittext.py
@@ -1,9 +1,10 @@
 import sys
 args = sys.argv
-
+#引数を変数に代入
 english = args[1]
 num = int(args[2])
-
+#リストを作成
 words = english.split()
-print(words[num - 1])
+#指定した単語を表示
+print(words[num - 1], end="")
 

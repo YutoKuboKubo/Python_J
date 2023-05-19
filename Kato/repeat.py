477c079d187fc85df99b5f084b9ed458bceec497
YoshitakaKato-web <kato.yoshitaka.sp@mynavi.jp>
2023-05-19 01:11:04 +0000

    fix repeat.py

--- Kato/repeat.py
+++ Kato/repeat.py
@@ -4,9 +4,12 @@ args = sys.argv
 input_num = int(args[1])
 sum_num = input_num + input_num
 
-while sum_num >= 100:
-    if sum_num == 100:
+while sum_num <= 100 or input_num == 100:
+    if sum_num == 100 or input_num == 100:
         print("Just 100!", end="")
+        break
+    elif input_num > 100:
+        print("Over!", end="")
         break
     sum_num = sum_num + input_num
 else:

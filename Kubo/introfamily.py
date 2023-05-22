27dc9fcdf85e93017a98719d5cea591ca7a0c40b
YutoKuboKubo <kubo.yuto.ik@mynavi.jp>
2023-05-22 06:34:23 +0000

    add myfamily.py

--- Kubo/introfamily.py
+++ Kubo/introfamily.py
@@ -2,7 +2,8 @@ from introduce import Intro
 
 
 class IntroFam(Intro):
-    def __init__(self, cat_name):
+    def __init__(self, name, age, cat_name):
+        super().__init__(name, age)
         self.cat_name = cat_name
 
     def cat_out(self):
---
+++ Kubo/myfamily.py
@@ -0,0 +1,6 @@
+from introfamily import IntroFam
+
+info = IntroFam("Bob", 21, "Cat")
+print(info.name_out())
+print(info.age_out())
+print(info.cat_out())

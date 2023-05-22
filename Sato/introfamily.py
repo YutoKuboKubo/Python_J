5eabbe898505884eb9e76937adc0149ac12efcbb
AkiraTasuku <sato.akira.nt@mynavi.jp>
2023-05-22 07:22:09 +0000

    オブジェクト指向2

--- Sato/introfamily.py
+++ Sato/introfamily.py
@@ -1,6 +1,10 @@
 import introduce
 
 class IntroFam(introduce.Intro):
-    def cat_out(self,cat_name):
-        cattxt = "飼い猫の名前は、" + str(cat_name) + "です"
+    def __init__(self, name, age,cat_name):
+        super().__init__(name, age)
+        self.cat_name = cat_name 
+
+    def cat_out(self):
+        cattxt = "飼い猫の名前は、" + str(self.cat_name) + "です"
         return cattxt
--- Sato/myfamily.py
+++ Sato/myfamily.py
@@ -5,8 +5,10 @@ name = sys.argv[1]
 age = sys.argv[2]
 cat_name = sys.argv[3]
 
-fam = introfamily.IntroFam(name,age)
+fam = introfamily.IntroFam(name,age,cat_name)
 
 print(fam.name_out())
 print(fam.age_out())
-print(fam.cat_out(cat_name))
+print(fam.cat_out())
+
+

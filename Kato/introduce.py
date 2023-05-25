0787a7e63a189d8f79d70d26780564490ccd5bc8
YoshitakaKato-web <kato.yoshitaka.sp@mynavi.jp>
2023-05-25 06:32:28 +0000

    add comment

--- Kato/introduce.py
+++ Kato/introduce.py
@@ -1,13 +1,15 @@
+#自己紹介のクラス
 class Intro:
+    #初期化
     def __init__(self, name, age):
         self.name = name
         self.age = age
-
+    #自分の名前を紹介する
     def name_out(self):
-        nametxt = "わたしの名前は、" + self.name + "です"
-        return nametxt
-    
+        name_txt = "わたしの名前は、" + self.name + "です"
+        return name_txt
+    #自分の年齢を紹介する
     def age_out(self):
-        agetxt = "年齢は、" + self.age + "歳です"
-        return agetxt
+        age_txt = "年齢は、" + self.age + "歳です"
+        return age_txt
         

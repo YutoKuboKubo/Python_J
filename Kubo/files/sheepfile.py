d02fc1a2a40f3d9a4a89d001bef02fbf98acae70
YutoKuboKubo <kubo.yuto.ik@mynavi.jp>
2023-05-25 02:51:31 +0000

    fixed sheepfile.py

--- Kubo/files/sheepfile.py
+++ Kubo/files/sheepfile.py
@@ -1,9 +1,15 @@
 import sys
+import re
 args = sys.argv
+
+
+with open("sheep.txt", mode="r", encoding="utf-8") as f:
+    last_line = f.readlines()[-1]
+    num = int(re.sub('\D', '', last_line))
 
 # ファイルを追記モードで開く
 with open("sheep.txt", mode="a", encoding="utf-8") as f:
     # 1から引数の値+1までを範囲に指定することで、変数iが1から引数の値まで代入される
     for i in range(1, int(args[1]) + 1):
-        message = f"ひつじが{i}匹\n"
+        message = f"ひつじが{i+num}匹\n"
         f.write(message)
